bda80ab0667515315ca3635275611da21d78f028
habibatulaulia <habibatulaulia76@gmail.com>
2024-03-04 11:10:11 +0700

    menambah firebase

--- main.js
+++ main.js
@@ -13,19 +13,20 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9uJSx87wLWMehUFmpdboN9sBM9s_tN1M",
-  authDomain: "insan-cemerlang-41b6b.firebaseapp.com",
-  projectId: "insan-cemerlang-41b6b",
-  storageBucket: "insan-cemerlang-41b6b.appspot.com",
-  messagingSenderId: "1057804782651",
-  appId: "1:1057804782651:web:bf911a5c4bff4f7bef201e"
+  
+  apiKey: "AIzaSyBbJDnmlNa2wObKWmQ6MLuXQ5KWbp0FDEA",
+  authDomain: "insan-cemerlang-8011b.firebaseapp.com",
+  projectId: "insan-cemerlang-8011b",
+  storageBucket: "insan-cemerlang-8011b.appspot.com",
+  messagingSenderId: "642542638808",
+  appId: "1:642542638808:web:8dae3c0d85e3be8c5c29ad"
 };
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export async function ambilDataPembeli() {
+export async function ambilDaftarPembeli() {
   const refDokumen = collection(db, "pembeli");
   const kueri = query(refDokumen, orderBy("nama"));
   const cuplikanKueri = await getDocs(kueri);
@@ -49,7 +50,7 @@ export function formatAngka(x) {
 
 export async function tambahDataPembli(nama, alamat, notlpon) {
   try {
-    const dokRef = await addDoc(collection(db, 'produk'), {
+    const dokRef = await addDoc(collection(db, 'pembeli'), {
       nama: nama,
       alamat: alamat,
       notlpon: notlpon
