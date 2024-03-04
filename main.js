e24b85a959212edd05751fe50a9be885ea62b1dc
habibatulaulia <habibatulaulia76@gmail.com>
2024-03-04 09:22:24 +0700

    menampilkan data pembeli

---
+++ main.js
@@ -0,0 +1,61 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9uJSx87wLWMehUFmpdboN9sBM9s_tN1M",
+  authDomain: "insan-cemerlang-41b6b.firebaseapp.com",
+  projectId: "insan-cemerlang-41b6b",
+  storageBucket: "insan-cemerlang-41b6b.appspot.com",
+  messagingSenderId: "1057804782651",
+  appId: "1:1057804782651:web:bf911a5c4bff4f7bef201e"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDataPembeli() {
+  const refDokumen = collection(db, "pembeli");
+  const kueri = query(refDokumen, orderBy("nama pembeli"));
+  const cuplikanKueri = await getDocs(kueri);
+  
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id, 
+      namapembeli: dok.data().namapembeli,
+      alamat: dok.data().alamat,
+      notlpon: dok.data().notlpon,
+    });
+  });
+  
+  return hasil;
+}
+
+export function formatAngka(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+export async function tambahDataPembli(namapembeli, alamat, notlpon) {
+  try {
+    const dokRef = await addDoc(collection(db, 'produk'), {
+      namapembeli: namapembeli,
+      alamat: alamat,
+      notlpon: notlpon
+    });
+    console.log('Berhasil menambah pembeli' + dokRef.id);
+  } catch (e) {
+    console.log('Gagal menambah pembeli' + e);
+  }
+}
